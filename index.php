23a9a8fa6d00078a145e019d2e95b969c0f20a3c
Benjamin <benjamin.suel1@etu.univ-lorraine.fr>
2022-12-01 09:10:19 +0100

    Ajout de la page recette

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    header('Location: /projet_boissons/src/aperiton.php');
+?>
---
+++ src/header.php
@@ -0,0 +1,46 @@
+<?php session_start();?>
+<header>
+    <!-- Importation du fichier style css -->
+    <link rel="stylesheet" href="../css/style.css" media="screen" type="text/css" />
+
+    <div id="top"> 
+        <div id="top_left">
+            <a href="aperiton.php"><h2>Aperiton</h2></a>
+        </div>
+        <div id="top_center">
+            <input type="text" placeholder="Rechercher une recette" name="recipeResearch" required>
+        </div>
+        <div id="top_right">
+            <!-- Le cas où l'utilisateur est connu -->
+            <?php if(isset($_SESSION['username'])): ?>
+                    <button>
+                        Mes favoris
+                    </button>   
+                    <button onclick="window.location.href = 'aperiton.php?deconnexion=true';">
+                        Deconnexion
+                        <?php 
+                            if(isset($_GET['deconnexion'])){ 
+                                if($_GET['deconnexion']==true){ 
+                                    session_unset();
+                                    header("location:aperiton.php");
+                                }
+                            }
+                        ?>
+                    </button>
+            <!-- Le cas où l'utilisateur est inconnu -->
+            <?php else: ?>
+                    <button onclick="window.location.href = 'login.php';">
+                        Mes favoris
+                    </button>   
+                    <button onclick="window.location.href = 'login.php';">
+                        Connexion
+                    </button>
+            <?php endif; ?>
+        </div>
+    </div>
+    <!-- La barre de navigation -->
+    <nav id="toolbar">
+        <a href="page1.html">Toutes nos recettes</a>
+        <a href="page2.html">Recette au hasard</a>
+    </nav>
+</header>
