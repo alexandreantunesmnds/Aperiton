fa4811a14f21bc155a5310e554cb04d85e649e44
Benjamin <benjamin.suel1@etu.univ-lorraine.fr>
2022-12-01 12:52:27 +0100

    Ajout de la préparation et des ingrédients dans la page recette

--- src/recette.php
+++ src/recette.php
@@ -15,16 +15,35 @@
     <?php 
         if(isset($_GET['nom'])){ 
             $recipe_title = $_GET['nom'];
+
+            /* On recherche la recette dans la bdd */ 
+            $mysqli=mysqli_connect('localhost', 'root', '','Boissons') or die("Erreur de connexion");
+            $requete = "SELECT ingredients, preparation FROM recettes WHERE nom = '$recipe_title'";
+            $exec_requete = mysqli_query($mysqli,$requete);
+            $reponse = mysqli_fetch_array($exec_requete);
         }
     ?>
     <div id="body">
         <div id="recipe">
             <span id=ariane>
-                <a href="aperiton.php">Accueil</a> > recette > <a href="#"><?php echo $recipe_title; ?></a>
+                <a href="aperiton.php">Accueil</a> > recettes > <a href="#"><?php echo $recipe_title; ?></a>
             </span>
             <div class="recipe">
                 <div id="recipe_title">
                     <h1><?php echo $recipe_title; ?></h1>
+                </div>
+                <div id="recipe_photo">
+                    <!-- mettre photo ici -->
+                </div>
+                <div id="recipe_ingredients">
+                    <hr>
+                    <h2>Ingrédients</h2>
+                    <?php echo $reponse['ingredients']; ?>
+                </div>
+                <div id="recipe_preparation">
+                    <hr>
+                    <h2>Préparation</h2>
+                    <?php echo $reponse['preparation']; ?>
                 </div>
             </div>
         </div>
