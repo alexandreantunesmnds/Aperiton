3397ffe475a9ca3f6b77877cbe3dd7601661e89e
Benjamin <benjamin.suel1@etu.univ-lorraine.fr>
2022-12-05 15:30:01 +0100

    Ajout de la recherche par mots clef (nom ou ingrédients)

--- src/header.php
+++ src/header.php
@@ -1,4 +1,16 @@
-<?php session_start();?>
+<?php session_start();
+    /* Requête permettant la recherche de recette à partir de mots clefs (ingrédients ou nom) */
+    /* Code à potentiellement déplacé */
+    $mysqli=mysqli_connect('localhost', 'root', '','Boissons') or die("Erreur de connexion");
+    $requete = "SELECT * FROM recettes ORDER BY id_recette DESC";
+    $all_recipe = mysqli_query($mysqli,$requete);
+
+    if(isset($_GET['search_recipe']) && !empty($_GET['search_recipe'])){
+        $recherche = htmlspecialchars($_GET['search_recipe']);
+        $requete = "SELECT nom, ingredients FROM recettes WHERE nom LIKE '%$recherche%' OR ingredients LIKE '%$recherche%' ORDER BY id_recette DESC";
+        $all_recipe = mysqli_query($mysqli,$requete);
+    }
+?>
 <header>
     <!-- Importation du fichier style css -->
     <link rel="stylesheet" href="../css/style.css" media="screen" type="text/css" />
@@ -8,8 +20,11 @@
             <a href="aperiton.php"><h2>Aperiton</h2></a>
         </div>
         <div id="top_center">
-            <input type="text" placeholder="Rechercher une recette" name="recipeResearch" required>
-        </div>
+            <form method="GET">
+                <input type="search" placeholder="Rechercher une recette" name="search_recipe" autocomplete="off">
+                <input type="submit" name="Rechercher">
+            </form>
+            </div>
         <div id="top_right">
             <!-- Le cas où l'utilisateur est connu -->
             <?php if(isset($_SESSION['username'])): ?>
@@ -45,3 +60,17 @@
         <a href="page2.html">Recette au hasard</a>
     </nav>
 </header>
+
+<body> <!-- A transférer dans une autre page -->
+    <?php
+    if(isset($_GET['search_recipe']) && !empty($_GET['search_recipe'])){
+        if( mysqli_num_rows($all_recipe)  > 0){
+            while($recipe = mysqli_fetch_array($all_recipe)){
+                echo $recipe['nom'];
+            }
+        }else{
+            echo "Aucune recette trouvé";
+        }
+    }
+    ?>
+</body>
