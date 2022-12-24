1d8ffc75abb6df8e7706e18f48edbf1f2c1aa649
Benjamin <benjamin.suel1@etu.univ-lorraine.fr>
2022-12-24 05:31:02 +0100

    Retrait du menu hamburger

--- src/header.php
+++ src/header.php
@@ -19,26 +19,8 @@
 
     <div id="top">
     <div id="top_left">
-    <nav>
-    <div class="navbar">
-        <div class="container nav-container">
-            <input class="checkbox" type="checkbox" name="" id="" />
-            <div class="hamburger-lines">
-              <span class="line line1"></span>
-              <span class="line line2"></span>
-              <span class="line line3"></span>
-            </div>  
-          <div class="menu-items">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">about</a></li>
-            <li><a href="#">blogs</a></li>
-            <li><a href="#">portfolio</a></li>
-            <li><a href="#">contact</a></li>
-          </div>
-        </div>
-      </div>
-    </nav> 
-</div>
+     
+    </div>
 
         <!-- Barre de recherche -->
         <div id="top_center">
